abcf566f21d1f4a7343c00182d479c90599ef554
talha22081992 <tla.freelancer@gmail.com>
2020-07-24 16:02:35 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Test"
+
+@app.route("/test")
+def test():
+    return "Test/test"
+
+# provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app, app_version="v0.1.2", app_config="staging")
+
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+run_simple(hostname="localhost", port=5000, application=dispatcher)
